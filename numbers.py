50ea3d0e4733df0b03f780c12904f3f24db377c3
DurgaSravani Kapilavai <32623233+sravanikd@users.noreply.github.com>
2018-02-04 17:41:34 -0600

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,37 @@
+- Casting one number to be float: float(3)/2; = 1.5 float(3) = 3.0;
+
+-The //  operator (two forward-slashes) performs "floor division" and truncates the decimal without rounding.
+
+# module for python 2 to import modules from python 3
+
+from __future__ import division
+
+# Finding power
+
+2 to the power of 3 == 2**3;
+
+# Order of operation is followed in this..
+
+# Assigning labels to objects
+
+- using equal sign: a=5; if we call a,it'll output 5
+
+##Rules to create labels: 
+1. Names can not start with a number.
+2. There can be no spaces in the name, use _ instead.
+3. Can't use any of these symbols :'",<>/?|\()!@#$%^&*~-+
+3. It's considered best practice (PEP8) that the names are lowercase.
+
+Using variable names can be a very useful way to keep track of different variables in Python. For example:
+
+-Use object names to keep better track of what's going on in your code!
+my_income = 100
+
+tax_rate = 0.1
+
+my_taxes = my_income*tax_rate
+
+-Show my taxes!
+my_taxes
+Out[46]:
+10.0
